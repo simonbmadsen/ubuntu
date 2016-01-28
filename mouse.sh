3a7e7c1c0331a64afc1edf7b612df1ba907c29ec
Simon Bønløkke Madsen <simonbmadsen@gmail.com>
2016-01-28 17:36:43 +0100

    flere filer

---
+++ mouse.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+xinput set-prop 10 'Device Accel Constant Deceleration' 1 
+xinput set-prop 11 'Device Accel Constant Deceleration' 1 
+xinput set-prop 10 'Device Accel Profile' -1
+xinput set-prop 11 'Device Accel Profile' -1
+xinput set-prop 10 'Device Accel Velocity Scaling' 1
+xinput set-prop 11 'Device Accel Velocity Scaling' 1 
+
---
+++ sbm-packages.sh
@@ -0,0 +1,15 @@
+#skype
+#dash-to-dock
+#chrome
+#mouse script
+#vmware player
+#vmware VIX SDK
+sudo add-apt-repository ppa:gns3/ppa
+sudo apt-get install network-manager-openconnect-gnome
+sudo apt-get install steam
+sudo apt-get install vim
+sudo apt-get install remmina
+sudo apt-get install gns3-gui
+sudo apt-get install socat
+sudo apt-get install evolution-ews
+sudo apt-get install evolution-mapi
